53443dd5fc0ddb385ec9d317382ca1799b76d1db
ppmonster111 <bosskak0987@gmail.com>
2025-01-31 18:39:13 +0700

    Remove unused _app.tsx file and enforce consistent casing in tsconfig.json

--- app/_app.tsx
+++
@@ -1,9 +0,0 @@
-// filepath: /C:/Users/p/Desktop/SunSeeker/pages/_app.tsx
-import '../styles/tailwind.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
