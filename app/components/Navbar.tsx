bd1efb51fc04a5a254d74070ab1ade1957b92886
ppmonster111 <bosskak0987@gmail.com>
2025-02-12 17:21:21 +0700

    fix dropdown

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -70,7 +70,7 @@ export default function Navbar() {
           {dropdownOpen && (
             <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20">
               <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">
-                My favourite
+                My profile
               </button>
               <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">
                 Log out
